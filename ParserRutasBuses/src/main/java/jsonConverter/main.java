d942dba14d1c1c8cf281b70bb82b7c20869880d7
Diego Mora <25236113+diego0209@users.noreply.github.com>
2017-11-05 17:35:18 -0600

    Corrección por problema del json
    Se comentaron líneas debido al error con el json

--- ParserRutasBuses/src/main/java/jsonConverter/main.java
+++ ParserRutasBuses/src/main/java/jsonConverter/main.java
@@ -39,10 +39,11 @@ public class main{
         // Convert to a JSON object to print data
         JsonParser jp = new JsonParser(); //from gson
         JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
-        JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
-        String distrito = rootobj.get("city").getAsString();
-        System.out.println(root.toString());
-        System.out.println(distrito);
+        System.out.print(request.getContent());
+        //JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
+        //String distrito = rootobj.get("city").getAsString();
+        //System.out.println(root.toString());
+        //System.out.println(distrito);
     }
 
     public void parser(){
